9a5316bb810ba6c6eb84de9cb6dfa544aa258308
unknown <tr.ywchiu@gmail.com>
2015-09-20 11:58:53 +0800

    add get

---
+++ fetchnews.java
@@ -0,0 +1,15 @@
+package appledaily;
+
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class fetchnews {
+	   public static void main(String[] args) throws IOException{
+		   Document doc;
+			doc = Jsoup.connect("http://www.appledaily.com.tw/realtimenews/section/new/").get();
+			String html = doc.html();
+			System.out.println(html);	   
+	   }
+}
