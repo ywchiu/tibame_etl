abdce7a7c1e0e570390e29d232edbc6f305852af
ywchiu <tr.ywchiu@gmail.com>
2015-12-05 12:52:26 +0800

    add fill 0

--- htmlpost.java
+++ htmlpost.java
@@ -1,22 +1,29 @@
 package appledaily;
 
 import java.io.IOException;
-
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class htmlpost {
-	public static void main(String[] args) throws IOException {
-		Document doc;
-		doc = Jsoup.connect("https://www.thsrc.com.tw/tw/TimeTable/SearchResult")
-				.data("StartStation", "977abb69-413a-4ccf-a109-0272c24fd490")
-				.data("EndStation", "fbd828d8-b1da-4b06-a3bd-680cdca4d2cd")
-				.data("SearchDate", "2015/11/07")
-				.data("SearchTime", "11:30")
-				.data("SearchWay", "DepartureInMandarin")
-				.post();
-		String html = doc.html();
-		System.out.println(html);
 
-	}
+	   public static void main(String[] args) throws IOException{
+		    Document doc;
+			doc = Jsoup.connect("https://www.thsrc.com.tw/tw/TimeTable/SearchResult")
+					.data("StartStation", "977abb69-413a-4ccf-a109-0272c24fd490")
+					.data("EndStation", "fbd828d8-b1da-4b06-a3bd-680cdca4d2cd")
+					.data("SearchDate", "2015/12/05")
+					.data("SearchTime", "11:00")
+					.data("SearchWay", "DepartureInMandarin")
+					.validateTLSCertificates(false)
+					.post();
+
+			String html = doc.html();
+			System.out.println(html);	   
+	   }
 }
