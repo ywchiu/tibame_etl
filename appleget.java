9c032d58fbe82be1566435bd38c9d440b471b99c
David <tr.ywchiu@gmail.com>
2016-06-17 10:57:01 +0800

    add appleget

--- appleget.java
+++ appleget.java
@@ -6,10 +6,11 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class appleget {
-	   public static void main(String[] args) throws IOException{
-		    Document doc;
-			doc = Jsoup.connect("http://www.appledaily.com.tw/realtimenews/section/new/").get();
-			String html = doc.html();
-			System.out.println(html);	   
-	   }
+public static void main(String args[]) throws IOException{
+	Document doc;
+	doc = Jsoup.connect("http://www.appledaily.com.tw/realtimenews/section/new/").get();  
+	String html = doc.html();
+	System.out.println(html);
+
+}
 }
